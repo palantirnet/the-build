3c884a61820676fe712629c14b86be21ad3e466c
Bec White <white@palantir.net>
2016-03-14 13:42:54 -0500

    Add a drupal-install target.

---
+++ conf/drushrc.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Project-specific drush configuration.
+ *
+ * This file can be used with `drush -c conf/drushrc.php`
+ */
+
+exec('git rev-parse --show-toplevel 2> /dev/null', $output);
+if (!empty($output)) {
+  $repo = current($output);
+
+  # Configure Drush for the current project.
+  $options['root'] = "@drupal.root@";
+  $options['uri'] = "@drupal.uri@";
+}
+
+$command_specific['site-install'] = array(
+  'sites-subdir' => '@drupal.sites_dir@',
+  'site-name' => '@drupal.site_name@',
+  'account-name' => 'admin',
+  'account-pass' => 'admin',
+);
+
+/**
+ * Using the flag "--structure-tables-key=common" on sql-dump and sql-sync will cause
+ * the structure but not the data to be dumped for these tables.
+ */
+$options['structure-tables']['common'] = array('cache', 'cache_*', 'history', 'search_*', 'sessions', 'watchdog');
