9af44e4f6a083ed7efe9a372fb07c7d9a2b1ddf8
Bec White <white@palantir.net>
2016-04-05 12:34:17 -0500

    Add file directory config to the settings.php.

--- conf/drupal/settings.php
+++ conf/drupal/settings.php
@@ -17,3 +17,6 @@ $config_directories[CONFIG_SYNC_DIRECTORY] = '@drupal.config_sync_directory@';
 $settings['hash_salt'] = '@hash_salt@';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+$settings['file_public_path'] = '@settings.file_public_path@';
+$settings['file_private_path'] = '@settings.file_private_path@';
