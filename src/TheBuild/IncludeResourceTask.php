7e78a33fb8a4bbf4759adcaa4e6e1c976fc780de
Bec White <white@palantir.net>
2017-01-23 12:46:33 -0600

    Create parent directories before copying or symlinking resources.

--- src/TheBuild/IncludeResourceTask.php
+++ src/TheBuild/IncludeResourceTask.php
@@ -60,6 +60,7 @@ class IncludeResourceTask extends \Task {
     }
 
     // Link or copy the source artifact.
+    $this->dest->getParentFile()->mkdirs();
     if ($this->mode == 'copy') {
       $this->log(sprintf("Copying '%s' to '%s'", $this->source->getPath(), $this->dest->getPath()));
       $this->source->copyTo($this->dest);
