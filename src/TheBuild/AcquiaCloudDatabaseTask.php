65a2620aa654611790b65c82d95f5dc267de56f3
Bec White <white@palantir.net>
2017-05-22 18:11:18 -0500

    Fetch the list of available backups.

--- src/TheBuild/AcquiaCloudDatabaseTask.php
+++ src/TheBuild/AcquiaCloudDatabaseTask.php
@@ -10,24 +10,36 @@ namespace TheBuild;
 use PhingFile;
 use BuildException;
 use FileSystem;
+use GuzzleHttp\Client as GuzzleHttpClient;
 
 
-class AcquiaCloudDatabase extends \Task {
+class AcquiaCloudDatabaseTask extends \Task {
 
   protected $acquiaCloudConfPath = '~/.acquia/cloudapi.conf';
   protected $acquiaCloudEmail = 'me@example.com';
   protected $acquiaCloudKey = 'fake_key';
   protected $acquiaCloudEndpoint = 'https://cloudapi.acquia.com/v1';
-  protected $acquiaSite = 'prod:some_site';
+  protected $acquiaRealm = 'prod';
+  protected $acquiaSite = 'some_site';
   protected $acquiaEnv = 'dev';
 
   protected $overwriteExisting = FALSE;
+
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  protected $client;
 
   /**
    * Init tasks.
    */
   public function init() {
     // @todo load Cloud API configuration from $this->confPath;
+
+    $this->client = new GuzzleHttpClient([
+      'base_uri' => $this->acquiaCloudEndpoint,
+      'auth' => [$this->acquiaCloudEmail, $this->acquiaCloudKey],
+    ]);
   }
 
 
@@ -46,20 +58,24 @@ class AcquiaCloudDatabase extends \Task {
 
 
   /**
-   * Verify that required attributes.
+   * Verify required attributes (?).
    */
   public function validate() {
-    if (strpos($this->acquiaSite, ':') === FALSE) {
-      throw new BuildException("Your site name must include a realm prefix, and will generally look like 'prod:mysite' or 'devcloud:mysite'.");
-    }
-
     if (!in_array($this->acquiaEnv, ['dev', 'test', 'prod'])) {
       throw new BuildException("env attribute must be either 'dev', 'test', or 'prod'", $this->env);
     }
   }
 
 
-  public function getAvailableBackups() {}
+  public function getAvailableBackups() {
+    $path = "sites/{$this->acquiaRealm}:{$this->acquiaSite}/envs/{$this->acquiaEnv}/dbs/{$this->acquiaSite}/backups.json";
+    $response = $this->client->get($path);
+    $backups = \GuzzleHttp\json_decode($response->getBody());
+    $this->log(print_r($backups, TRUE));
+
+    
+  }
+
   public function selectMostRecent() {}
   public function deleteExistingBackup() {
     if ($this->overwriteExisting && $this->dest->exists()) {
