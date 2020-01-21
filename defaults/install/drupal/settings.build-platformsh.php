6dffb027f1c303d205fe34df61769baf9d73da87
Bec White <white@palantir.net>
2020-01-21 16:45:49 -0600

    For Platform.sh, the tmp directory is configured in .platform.app.yml, so update our Drupal settings to use that.

--- defaults/install/drupal/settings.build-platformsh.php
+++ defaults/install/drupal/settings.build-platformsh.php
@@ -6,7 +6,9 @@
  */
 
 $settings['config_sync_directory'] = '${drupal.site.config_sync_directory}';
-$settings['file_temp_path'] = '${drupal.site.settings.file_temp_path}';
+
+// Use the temporary directory set up in .platform.app.yaml
+$settings['file_temp_path'] = '/tmp';
 
 // Enable/disable config_split configurations.
 if (isset($_ENV['PLATFORM_BRANCH'])) {
