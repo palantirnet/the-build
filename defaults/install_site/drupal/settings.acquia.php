f15aee891ea60dbb1dacddffa1148269ecdb0519
Jes Constantine <constantine@palantir.net>
2023-07-13 11:25:55 -0600

    Don't touch settings file which is being removed anyway
    This was a temporary and unnecessary change

--- defaults/install_site/drupal/settings.acquia.php
+++ defaults/install_site/drupal/settings.acquia.php
@@ -15,10 +15,4 @@ $settings['cache']['default'] = 'cache.backend.database';
 // Include the Acquia database connection and other config.
 if (file_exists('/var/www/site-php')) {
   require "/var/www/site-php/{$_ENV['AH_SITE_GROUP']}/{$_ENV['AH_SITE_GROUP']}-settings.inc";
-
-  // Memcached settings for Acquia Hosting
-  $memcache_settings_file = DRUPAL_ROOT . "/../vendor/acquia/memcache-settings/memcache.settings.php";
-  if (file_exists($memcache_settings_file)) {
-    require_once $memcache_settings_file;
-  }
 }
