e7c9965421e64a73913ade42de3e8ee7d45eea20
byrond <duvall@palantir.net>
2023-11-17 14:03:48 -0500

    update phpmd command in code-review.sh

--- code-review.sh
+++ code-review.sh
@@ -9,7 +9,7 @@ echo "-------------"
 vendor/bin/phpcs defaults/standard/modules --standard="Drupal,DrupalPractice" -n --extensions="php,module,inc,install,test,profile,theme"
 echo "Running PHPMD on modules"
 echo "-------------"
-vendor/bin/phpmd defaults/standard/modules text defaults/standard/phpmd.xml "php,inc,module,theme,profile,install,test"
+vendor/bin/phpmd defaults/standard/modules text defaults/standard/phpmd.xml --suffixes php,inc,module,theme,profile,install,test
 echo "Running PHPStan on modules"
 echo "---------------"
 vendor/bin/phpstan analyse defaults/standard/modules --level=2
@@ -24,7 +24,7 @@ echo "-------------"
 vendor/bin/phpcs src --standard="Drupal,DrupalPractice" -n --extensions="php,module,inc,install,test,profile,theme"
 echo "Running PHPMD on tasks"
 echo "-------------"
-vendor/bin/phpmd src text defaults/standard/phpmd.xml "php,inc,module,theme,profile,install,test"
+vendor/bin/phpmd src text defaults/standard/phpmd.xml --suffixes php,inc,module,theme,profile,install,test
 echo "Running PHPStan on tasks"
 echo "---------------"
 vendor/bin/phpstan analyse src --level=2
